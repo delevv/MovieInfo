59bba34652c765837e05c15e6980a104acd84a33
delev <delyan1@mail.bg>
2021-02-12 15:46:59 +0200

    Implement getGenres in moveiService

--- movie-info/src/app/core/services/movie.service.ts
+++ movie-info/src/app/core/services/movie.service.ts
@@ -5,12 +5,14 @@ import { HttpClient } from '@angular/common/http';
 import { MovieList } from 'src/app/shared/interfaces/movie-list';
 import { environment } from 'src/environments/environment';
 import { Movie } from 'src/app/shared/interfaces/movie';
+import { Genre } from 'src/app/shared/interfaces/genre';
 
 const API_KEY: string = environment.MovieDb.API_KEY;
-const API_URL: string = 'https://api.themoviedb.org/3/movie';
-const popularUrl: string = `${API_URL}/popular`;
-const topRatedUrl: string = `${API_URL}/top_rated`;
-const nowPlaying: string = `${API_URL}/now_playing`;
+const moviesUrl: string = 'https://api.themoviedb.org/3/movie';
+const genresURL: string = 'https://api.themoviedb.org/3/genre/movie/list';
+const popularUrl: string = `${moviesUrl}/popular`;
+const topRatedUrl: string = `${moviesUrl}/top_rated`;
+const nowPlaying: string = `${moviesUrl}/now_playing`;
 
 @Injectable({
   providedIn: 'root',
@@ -37,6 +39,10 @@ export class MovieService {
   }
 
   getDetails(id: number): Observable<Movie> {
-    return this.http.get<Movie>(`${API_URL}/${id}?api_key=${API_KEY}`);
+    return this.http.get<Movie>(`${moviesUrl}/${id}?api_key=${API_KEY}`);
+  }
+
+  getGenres(): Observable<Genre[]> {
+    return this.http.get<Genre[]>(`${genresURL}?api_key=${API_KEY}`);
   }
 }
